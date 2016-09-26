68a61102121ec376ae49c835fa2c548d7fd955c9
Maxime Rouiller <angelzerosoft@gmail.com>
2016-09-26 16:24:15 -0400

    Updated sdk. Corrected _references.js

--- src/Novaweb.Invoicing.UI/wwwroot/_references.js
+++ src/Novaweb.Invoicing.UI/wwwroot/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
-/// <reference path="dist/app.min.js" />
 /// <reference path="js/app.js" />
+/// <reference path="lib/angular/angular.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
