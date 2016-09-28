e4f832c2c83529eafe07ae595021dfc8d2bb5ff8
Maxime Rouiller <angelzerosoft@gmail.com>
2016-09-27 21:04:36 -0400

    Added base code to generate invoices

---
+++ src/GenerateInvoice/CloudStorage.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Invoicing.Model;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+namespace GenerateInvoice
+{
+    public class CloudStorage
+    {
+        private readonly CloudTable _invoiceTable;
+        private readonly CloudTable _projectTables;
+        private readonly CloudTable _timeEntryTable;
+        private readonly CloudTable _clientTable;
+
+        public CloudStorage(string connectionString, string containerName)
+        {
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            var tableClient = storageAccount.CreateCloudTableClient();
+            _clientTable = tableClient.GetTableReference("Client");
+            _timeEntryTable = tableClient.GetTableReference("TimeEntries");
+            _projectTables = tableClient.GetTableReference("Projects");
+            _invoiceTable = tableClient.GetTableReference("Invoices");
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _clientTable.CreateIfNotExistsAsync();
+            await _timeEntryTable.CreateIfNotExistsAsync();
+            await _projectTables.CreateIfNotExistsAsync();
+            await _invoiceTable.CreateIfNotExistsAsync();
+        }
+
+
+        public async Task GenerateInvoices(string partitionKey)
+        {
+            var monthlyTimeEntries = TableQuery.CombineFilters(
+                TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionKey),
+                TableOperators.And,
+                TableQuery.CombineFilters(TableQuery.GenerateFilterConditionForDate("At", QueryComparisons.GreaterThanOrEqual, new DateTime(2016, 9, 1)),
+                TableOperators.And, TableQuery.GenerateFilterConditionForDate("At", QueryComparisons.LessThanOrEqual, new DateTime(2016, 9, 30)))
+                );
+            var entries = await _timeEntryTable.ExecuteQuerySegmentedAsync(new TableQuery<TimeEntry>().Where(monthlyTimeEntries), new TableContinuationToken());
+            var timeEntries = entries.Results;
+        }
+
+    }
+}
---
+++ src/GenerateInvoice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace GenerateInvoice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task.WaitAll(Run(args));
+        }
+
+        public static async Task Run(string[] args)
+        {
+
+            Console.WriteLine("Starting Raw data transformation...");
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(PlatformServices.Default.Application.ApplicationBasePath)
+                .AddCommandLine(args)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.production.json", true)
+                .Build();
+
+
+            var storage = new CloudStorage(configuration["storageAccount"], "toggl-rawdata");
+            Console.WriteLine("Initializing storage...");
+            await storage.InitializeAsync();
+
+            Console.WriteLine("Generating invoices...");
+            await storage.GenerateInvoices("Novaweb");
+        }
+
+    }
+}
